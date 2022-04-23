f88c1cb02d71e327882e240683d5753c021d3734
Meenal-06 <96614365+Meenal-06@users.noreply.github.com>
2022-04-23 17:06:20 +0530

    factorial

---
+++ factorial.py
@@ -0,0 +1,8 @@
+print('This program is find factorial of user defined integer')
+n=int(input('Enter a number to find factorial'))
+sum=1
+for i in range (1,n+1):
+    sum=sum*i
+    
+print('Factorial of given number= ',sum)
+
