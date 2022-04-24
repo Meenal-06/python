b58a63e2bde3188ba0b6c6095e0f766c5e736207
Meenal-06 <96614365+Meenal-06@users.noreply.github.com>
2022-04-24 18:15:10 +0530

    lcm of 3 number

---
+++ divisorofnum.py
@@ -0,0 +1,6 @@
+import math
+print('this program ask the user to enter a number and then LCM')            
+a1,a2,a3=int(input('\nEnter first value' )),int(input('\nEnter second value' )),int(input('\nEnter third value' ))
+x=math.lcm(a1,a2,a3)
+print('Lcm of number',x)
+
