861fb8194592323fa57f5c511a53dbc52a622615
Meenal-06 <96614365+Meenal-06@users.noreply.github.com>
2022-04-28 14:57:05 +0530

    string

---
+++ stringslic.py
@@ -0,0 +1,29 @@
+#string slicing
+"""i="I am fine"
+#x=i[4:8]
+#x=i[ :4]
+x=i[2: ]
+print(x)"""
+#string spliting
+"""i="i am meenal patidar"
+y=i.split(" ")
+print(y)"""
+#string concatination
+"""x="Meenal"
+y="Patidar"
+print(x+y)
+print(x,"",y)
+"""
+#sring multiplication
+'''a="meenal"
+print(a*5)'''
+#pattern by strig mul
+a="*"
+for i in range(1,10,1):
+    print(a*i)
+#reverse pattern
+''''for i in range(10,0,-1):
+    print(a*i)'''
+
+
+
