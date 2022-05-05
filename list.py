32402ee9c3fcba6b1c288bd33d196ef0387bbd88
Meenal-06 <96614365+Meenal-06@users.noreply.github.com>
2022-05-05 22:31:52 +0530

    list

---
+++ list.py
@@ -0,0 +1,15 @@
+#create list
+'''a=[1,2,3,4,5]
+print('List=',a)
+a.append(6)
+a.insert(3,4)
+print('List after append and insertion at index 3 is=',a)
+b=a.index(3)
+print('Index of 3 is=',b)
+'''
+a=['Meenal patidar',19,1234567890,'IIPS','meenalpatidar06@gmail.com']
+x=len(a)
+for i in a:
+    print(i)
+#for i in range(x):
+   # print(a[i])
