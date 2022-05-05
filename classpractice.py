20039cd748049c162c9c0b5d3a41f2ace21d22be
Meenal-06 <96614365+Meenal-06@users.noreply.github.com>
2022-05-05 22:23:50 +0530

    class and object

---
+++ classpractice.py
@@ -0,0 +1,10 @@
+class cars:
+    def __init__(self,b_name,model,types):
+        self.b_name=b_name
+        self.model=model
+        self.types=types
+    
+car1=cars("Maruti","ciaz","Sedan")
+c1=car1.model
+print('Model of car1 =',c1)
+
